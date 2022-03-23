2b595e82dc173fc2152b6b925e03cfb94e5007bb
Scotrraaj Gopal <scotgopal@gmail.com>
2022-03-23 23:05:21 +0800

    Optimizer and scheduler

--- utils.py
+++ utils.py
@@ -1,11 +1,25 @@
 import numpy as np
 from skimage.segmentation import mark_boundaries
 import matplotlib.pyplot as plt
+from torch import optim
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
 
 def show_img_mask(img, mask):
-    img_mask = mark_boundaries(
-        np.array(img),
-        np.array(mask),
-        outline_color=(0,1,0)
-        )
+    img_mask = mark_boundaries(np.array(img), np.array(mask), outline_color=(0, 1, 0))
     plt.imshow(img_mask)
+
+
+def get_optim(model):
+    """Return Adam optimizer"""
+    return optim.Adam(model.parameters(), lr=3e-4)
+
+
+def get_lr_scheduler(optimizer):
+    """Return ReduceLROnPlateau scheduler"""
+    return ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=20, verbose=1)
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
