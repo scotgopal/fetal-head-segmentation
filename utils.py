b6b1f8dd3ee3b804ab6045fa0e21066d2e84acb3
Scotrraaj Gopal <scotgopal@gmail.com>
2022-03-25 18:26:46 +0800

    Add method to compare state dicts

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from skimage.segmentation import mark_boundaries
 import matplotlib.pyplot as plt
+import torch
 from torch import optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 
@@ -23,3 +24,20 @@ def get_lr_scheduler(optimizer: optim.Optimizer):
 def get_lr(optimizer: optim.Optimizer):
     for param_group in optimizer.param_groups:
         return param_group["lr"]
+
+
+def compare_state_dicts(state_dict1, state_dict2):
+    """Check the similarity between two Torch state_dicts"""
+    state_dicts_differ = 0
+    for key_item_1, key_item_2 in zip(state_dict1.items(), state_dict2.items()):
+        if torch.equal(key_item_1[1], key_item_2[1]):
+            pass
+        else:
+            state_dicts_differ += 1
+            if key_item_1[0] == key_item_2[0]:
+                print(f"Mismatch found at {key_item_1[0]}")
+            else:
+                raise Exception
+
+    if state_dicts_differ == 0:
+        print("State_dicts match perfectly")
